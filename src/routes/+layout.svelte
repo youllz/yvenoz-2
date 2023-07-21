5980f2a9996af3d14c207965946548d0e3c55ac8
youllz <youssoufzerbo01@gmail.com>
2023-07-21 23:06:47 +0000

    init setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.scss';
+</script>
+
+<header />
+
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	main {
+		min-height: 100dvh;
+		width: 100%;
+		background-color: red;
+	}
+</style>
